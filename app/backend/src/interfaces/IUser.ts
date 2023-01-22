09c7c9677abb5a3bbe8a0b8055e877563ea39471
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-22 10:08:39 -0300

    docs: Create IUser interface and ILogin interface

---
+++ app/backend/src/interfaces/IUser.ts
@@ -0,0 +1,7 @@
+export default interface IUser {
+  id: number;
+  password: string;
+  username: string;
+  email: string;
+  role: string;
+}
---
+++ app/backend/src/interfaces/index.ts
@@ -0,0 +1,7 @@
+import IUser from './IUser';
+import ILogin from './ILogin';
+
+export {
+  IUser,
+  ILogin,
+};
