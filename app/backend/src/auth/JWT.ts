7f59344c762b37fb872ddef22ddeb969c9ec2c0a
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-22 10:11:53 -0300

    refactor: Rename JWT class

--- app/backend/src/auth/JWT.ts
+++ app/backend/src/auth/JWT.ts
@@ -1,13 +1,13 @@
 import * as jwt from 'jsonwebtoken';
 
-export default class JWT {
+export default class Token {
   private _secret: jwt.Secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
   private _jwtConfig: jwt.SignOptions = {
     algorithm: 'HS256',
     expiresIn: '1h',
   };
 
-  public createToken(userEmail: string): string {
+  public create(userEmail: string): string {
     const token = jwt.sign({ data: { email: userEmail } }, this._secret, this._jwtConfig);
     return token;
   }
