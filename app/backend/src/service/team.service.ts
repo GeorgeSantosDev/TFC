b538feb640381a08468205d6c725a7d90c121fb8
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-23 18:27:28 -0300

    refactor: Delete unnecessary exclude clause

--- app/backend/src/service/team.service.ts
+++ app/backend/src/service/team.service.ts
@@ -3,13 +3,13 @@ import Team from '../database/models/team.model';
 
 export default class TeamService {
   static async getAll(): Promise<ITeam[]> {
-    const response = await Team.findAll({ attributes: { exclude: ['password'] } });
+    const response = await Team.findAll();
 
     return response;
   }
 
   static async getById(id: number): Promise<ITeam | null> {
-    const response = await Team.findByPk(id, { attributes: { exclude: ['password'] } });
+    const response = await Team.findByPk(id);
 
     return response;
   }
