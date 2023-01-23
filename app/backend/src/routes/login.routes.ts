160d703845247b5a4d6cec1843ee2ddab6a044ee
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-23 16:24:28 -0300

    feat: Create get route for login

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,14 +1,22 @@
 import { Router } from 'express';
 import { LoginController } from '../controller';
 import LoginValidation from '../middlewares/LoginValidation';
+import Token from '../auth/JWT';
 
 const loginRouter = Router();
+const token = new Token();
 
 loginRouter.post(
   '/',
   (req, res, next) => LoginValidation.fieldsValidation(req, res, next),
   (req, res, next) => LoginValidation.credentialValidation(req, res, next),
   (req, res, next) => LoginController.login(req, res, next),
+);
+
+loginRouter.get(
+  '/validate',
+  (req, res, next) => token.validate(req, res, next),
+  (req, res, next) => LoginController.validateRole(req, res, next),
 );
 
 export default loginRouter;
