fc70a470354cf895f06d908a75ce0e587d27d39b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-23 16:22:42 -0300

    feat: Create method validate in Token class

--- app/backend/src/auth/JWT.ts
+++ app/backend/src/auth/JWT.ts
@@ -1,4 +1,7 @@
 import * as jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../utils/HttpException';
+import { IToken } from '../interfaces';
 
 export default class Token {
   private _secret: jwt.Secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
@@ -7,8 +10,32 @@ export default class Token {
     expiresIn: '1h',
   };
 
-  public create(userEmail: string): string {
-    const token = jwt.sign({ data: { email: userEmail } }, this._secret, this._jwtConfig);
+  create(userEmail: string, role: string): string {
+    const token = jwt.sign({ data: { email: userEmail, role } }, this._secret, this._jwtConfig);
+
     return token;
+  }
+
+  validate(req: Request, _res: Response, next: NextFunction): void {
+    const token = req.header('Authorization');
+
+    if (!token) {
+      throw new HttpException(401, 'Token not found');
+    }
+
+    const verify = jwt.verify(token, this._secret);
+
+    if (!verify) {
+      throw new HttpException(401, 'Expired or invalid token');
+    }
+
+    try {
+      const decodedToken = jwt.decode(token) as unknown as IToken;
+      req.body.role = decodedToken.data.role;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
   }
 }
