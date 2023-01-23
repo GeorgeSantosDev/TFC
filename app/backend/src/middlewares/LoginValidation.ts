423b02697da0c3adbbddbff2d4184cb357647557
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-23 09:30:07 -0300

    feat: Create middleware fot validate login

---
+++ app/backend/src/middlewares/LoginValidation.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { ILogin } from '../interfaces';
+import HttpException from '../utils/HttpException';
+import { LoginService } from '../service';
+import Crypt from '../utils/Bcrypt';
+
+export default class LoginValidation {
+  static fieldsValidation(req: Request, _res: Response, next: NextFunction): void {
+    try {
+      const { email, password } = req.body as ILogin;
+
+      if (!email || !password) {
+        throw new HttpException(400, 'All fields must be filled');
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async credentialValidation(
+    req: Request,
+    _res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const { email, password } = req.body as ILogin;
+
+      const response = await LoginService.getUser(email);
+
+      if (!response || !Crypt.verify(password, response.password)) {
+        throw new HttpException(401, 'Incorrect email or password');
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
