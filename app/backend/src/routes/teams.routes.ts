86047d438d265daa7cb7f5ac40365849c4065792
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-23 19:31:37 -0300

    refactor: Remove token validation

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
 import { TeamController } from '../controller';
-import Token from '../auth/JWT';
 
 const teamsRouter = Router();
-const token = new Token();
 
 teamsRouter.get(
   '/',
-  (req, res, next) => token.validate(req, res, next),
   (req, res, next) => TeamController.getAll(req, res, next),
 );
 
 teamsRouter.get(
   '/:id',
-  (req, res, next) => token.validate(req, res, next),
   (req, res, next) => TeamController.getById(req, res, next),
 );
 
