f9a3fa06dcdf4adb361a15f113ad73dfb1e327a9
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-25 20:34:08 -0300

    feat: Create compare function to sort method

---
+++ app/backend/src/utils/CompareFunction.ts
@@ -0,0 +1,15 @@
+import { ILeaderBoard } from '../interfaces';
+
+export default function compareFn(a: ILeaderBoard, b: ILeaderBoard): number {
+  if (a.totalPoints > b.totalPoints) return -1;
+  if (a.totalPoints < b.totalPoints) return 1;
+  if (a.totalVictories > b.totalVictories) return -1;
+  if (a.totalVictories < b.totalVictories) return 1;
+  if (a.goalsBalance > b.goalsBalance) return -1;
+  if (a.goalsBalance < b.goalsBalance) return 1;
+  if (a.goalsFavor > b.goalsFavor) return -1;
+  if (a.goalsFavor < b.goalsFavor) return 1;
+  if (a.goalsOwn > b.goalsOwn) return 1;
+
+  return -1;
+}
