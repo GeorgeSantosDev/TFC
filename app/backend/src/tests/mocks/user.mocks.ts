95a115575a4cef97e8323d844bfbc4a76eba0d81
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-25 14:04:54 -0300

    chore: Commit to run trybe evaluator again

--- app/backend/src/tests/mocks/user.mocks.ts
+++ app/backend/src/tests/mocks/user.mocks.ts
@@ -1,27 +1,27 @@
 const loginBody = {
   email: 'admin@admin.com',
   password: 'secret_admin',
-}
+};
 
 const loginBodyNoEmail = {
   email: '',
   password: 'secret_admin'
-}
+};
 
 const loginBodyNoPassword ={
   email: 'admin@admin.com',
   password: '',
-}
+};
 
 const loginBodyInvalidEmail = {
   email: 'teste@teste.com',
   password: 'secret_admin',
-}
+};
 
 const loginBodyInvalidPassword = {
   email: 'admin@admin.com',
   password: '123456',
-}
+};
 
 const user = {
   id: 1,
@@ -29,7 +29,7 @@ const user = {
   role: 'Admin',
   email: 'admin@admin.com',
   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
-}
+};
 
 export {
   loginBody,
@@ -38,4 +38,4 @@ export {
   loginBodyNoPassword,
   loginBodyInvalidEmail,
   loginBodyInvalidPassword
-}
+};
